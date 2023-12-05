7a14baf8394d70d82a0757d274720c3d8c043d7f
Bertl-81W <78581189+Bertl-81W@users.noreply.github.com>
2023-12-04 21:32:28 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './components/Header';
+import './style.css';
+import Meme from './components/Meme';
+
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+           <Header />
+           <Meme />
+      </div>      
+    </>
+  )
+}
+
+export default App
